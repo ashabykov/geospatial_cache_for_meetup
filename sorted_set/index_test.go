a5b09967bff84431308807da69dcdb8d97fbf975
ashabykov <a.shabykov@indriver.com>
2025-02-15 22:27:58 +0500

    added test

--- sorted_set/index_test.go
+++ sorted_set/index_test.go
@@ -9,6 +9,69 @@ import (
 	"github.com/ashabykov/geospatial_cache_for_meetup/location"
 )
 
+func TestIndex_Remove(t *testing.T) {
+	tests := []struct {
+		name      string
+		from, to  location.Timestamp
+		target    location.Location
+		locations []location.Location
+	}{
+		{
+			name: "remove from index",
+			from: location.Timestamp(time.Now().Add(-6 * time.Minute).Unix()),
+			to:   location.Timestamp(time.Now().Add(1 * time.Minute).Unix()),
+			target: location.NewLocation(
+				"awesome-3",
+				location.Timestamp(
+					time.Now().Add(-5*time.Minute).Unix(),
+				),
+				45.786877,
+				47.679879,
+			),
+			locations: []location.Location{
+				location.NewLocation(
+					"awesome-1",
+					location.Timestamp(
+						time.Now().Add(-15*time.Minute).Unix(),
+					),
+					45.68878,
+					47.57867,
+				),
+				location.NewLocation(
+					"awesome-2",
+					location.Timestamp(
+						time.Now().Add(-10*time.Minute).Unix(),
+					),
+					45.467467,
+					47.456656,
+				),
+				location.NewLocation(
+					"awesome-3",
+					location.Timestamp(
+						time.Now().Add(-5*time.Minute).Unix(),
+					),
+					45.786877,
+					47.679879,
+				),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			idx := New()
+
+			for _, loc := range tt.locations {
+				idx.Add(loc)
+			}
+
+			idx.Remove(tt.target)
+
+			assert.Equal(t, 0, len(idx.Read(tt.from, tt.to)))
+		})
+	}
+}
+
 func TestIndex_Read(t *testing.T) {
 	tests := []struct {
 		name      string
@@ -95,13 +158,13 @@ func TestIndex_Read(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			index := New()
+			idx := New()
 
 			for _, loc := range tt.locations {
-				index.Add(loc)
+				idx.Add(loc)
 			}
 
-			assert.Equal(t, tt.expected, index.Read(tt.from, tt.to))
+			assert.Equal(t, tt.expected, idx.Read(tt.from, tt.to))
 		})
 	}
 }
